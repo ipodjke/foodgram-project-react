83bc7cef064bef31de7e0004b0fd27dcf51babd1
Андрей Белов <Andreyviper@yandex.ru>
2021-10-27 16:17:44 +0300

    add test git workflow

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from djoser.conf import settings
-from djoser.serializers import (UserCreateSerializer
-                                as DjoserUserCreateSerializer)
+from djoser.serializers import \
+    UserCreateSerializer as DjoserUserCreateSerializer
 from djoser.serializers import UserSerializer as DjoserUserSerializer
 from recipes.models import Recipe
 from rest_framework import serializers
